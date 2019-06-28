01f035454cf176d028b0a39fbc57d2a757d0c654
cagri coltekin <ccoltekin@sfs.uni-tuebingen.de>
2019-06-28 15:38:55 +0200

    Fix missing lemmas.

--- scripts/sortf.py
+++ scripts/sortf.py
@@ -4,7 +4,7 @@ import sys
 for line in sys.stdin:
     line_s = line.strip().split('\t')
     if len(line_s) > 6:
-        line_s[5] = '|'.join(sorted(line_s[5].split('|')))
+        line_s[5] = '|'.join(sorted(line_s[5].split('|'), key=lambda s: s.lower()))
         print('\t'.join(line_s))
     else:
         print(line, end="")
